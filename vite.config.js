b76182c0bbd380587e5ca859335fdea5d909f681
wellanmini <sunkeeper316@gmail.com>
2026-07-30 14:03:07 +0800

    顯示

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 這裡的名稱必須和您 GitHub 儲存庫 (Repository) 的名稱完全一致！
+  // 如果您的 Repo 名稱是 sunkeeper_studio_website，就保留這樣。
+  // 如果是其他的，請改成 '/您的Repo名稱/'
+  base: '/sunkeeper_studio_website/',
 })
